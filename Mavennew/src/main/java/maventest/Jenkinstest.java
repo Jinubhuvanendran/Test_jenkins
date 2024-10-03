33ef268d76b1cf38a84a9c04049cf0c763979643
Mad_muzafir <Mad_muzafir@LAPTOP-KBSBS314>
2024-10-03 19:50:41 +0530

    first jenkins project

---
+++ Mavennew/src/main/java/maventest/Jenkinstest.java
@@ -0,0 +1,13 @@
+package maventest;
+
+public class Jenkinstest {
+	public void display() {
+		System.out.println("hello world");
+	}
+
+	public static void main(String[] args) {
+		Jenkinstest obj=new Jenkinstest();
+		obj.display();
+	}
+
+}
